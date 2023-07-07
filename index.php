7259d06a710ae02ae682ff027fee55d895cafa7b
phin <112971806+phinwww@users.noreply.github.com>
2023-07-07 17:44:17 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>file.haus</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <main>
+            <h1>file.haus</h1>
+            <p>
+                <input type="file" id="filetoupload"><br>
+                <label for="filetoupload">max 30gb</label>
+            </p>
+            <p>
+                <progress id="upload-progress"></progress>
+            </p>
+            <p id="message"></p>
+            <p>donate to our XMR address to help keep free, fast, and anonymous filesharing alive: 471fHHoNZi2dccVGyxNs8F8tWQUMg3Wy6iF8PhBZBs2MJ3TSSS8u3UhHiGBLNAhFJxayKvptgkicuSL6C7bdMKDWEECEvpR</p>
+            <p>2023-7-7: the new uploader (with the progress bar) is here, if you want to use the no-js version we have it up and always will.</p>
+            <p><a href="https://file.haus/nojs.php">javascript-less uploader</a> / <a href="https://file.haus">clearnet</a> / <a href="http://fileha3hjvudqs2moembtaegnpjo5ptec3tomf5dlxx6ydwtajkkahyd.onion">tor</a></p>
+        </main>
+        <script src="file-upload-progress-bar.js"></script>
+    </body>
+</html>
---
+++ nojs.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>file.haus</title>
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+<link rel="stylesheet" href="main.css">
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript" src="jquery.form.min.js"></script>
+<script type="text/javascript" src="upload_progress.js"></script>
+</head>
+<body>
+<center>
+<h1>file.haus</h1>
+<p>max 30gb file limit</p>
+<p><form id="uploadForm" action="https://file.haus/cdn1" method="post" enctype="multipart/form-data"></p>
+  <p>select file to upload:</p>
+  <p><input type="file" name="filetoupload" id="filetoupload"></p>
+  <p><input type="submit" value="Upload" name="submit" onclick='upload_image();'></p>
+</form>
+<p>donate to our XMR address to help keep free, fast, and anonymous filesharing alive: 471fHHoNZi2dccVGyxNs8F8tWQUMg3Wy6iF8PhBZBs2MJ3TSSS8u3UhHiGBLNAhFJxayKvptgkicuSL6C7bdMKDWEECEvpR</p>
+<p><a href=https://file.haus>clearnet</a> / <a href=http://fileha3hjvudqs2moembtaegnpjo5ptec3tomf5dlxx6ydwtajkkahyd.onion>tor</a></p>
+</center>
+</body>
+</html>
